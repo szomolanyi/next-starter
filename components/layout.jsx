7da813b387de0154487feb4ffacbc5b656491d5f
Robert Szomolanyi <szomolanyi@gmail.com>
2019-05-22 12:07:02 +0200

    eslint

--- components/layout.jsx
+++ components/layout.jsx
@@ -5,20 +5,19 @@ import Head from 'next/head';
 import AppErrorModal from './AppErrorModal';
 import Header from './header';
 
-const Layout = ({ children }) => {
-  return (
-    <React.Fragment>
-      <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous" />
-      </Head>
-      <AppErrorModal>
-        <Header />
-        <div className="container">
-          {children}
-        </div>
-      </AppErrorModal>
-    </React.Fragment>
-)};
+const Layout = ({ children }) => (
+  <React.Fragment>
+    <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous" />
+    </Head>
+    <AppErrorModal>
+      <Header />
+      <div className="container">
+        {children}
+      </div>
+    </AppErrorModal>
+  </React.Fragment>
+);
 
 export default Layout;
--- models/users.js
+++ models/users.js
@@ -1,25 +1,25 @@
-const mongoose=require("mongoose")
-const bcrypt = require('bcrypt')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 }, {
-  timestamps: true
-})
+  timestamps: true,
+});
 
 userSchema.pre('save', function save(next) {
-  const user = this
+  const user = this;
   bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err) }
-    console.log({salt, u:user.password})
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) { return next(err) }
-      user.password = hash
-      next();
-    })
-  })
-})
+    if (err) { return next(err); }
+    console.log({ salt, u: user.password });
+    return bcrypt.hash(user.password, salt, (err2, hash) => {
+      if (err2) { return next(err2); }
+      user.password = hash;
+      return next();
+    });
+  });
+});
 userSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
     cb(err, isMatch);
@@ -27,7 +27,6 @@ userSchema.methods.comparePassword = function comparePassword(candidatePassword,
 };
 
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports=User
-
+module.exports = User;
