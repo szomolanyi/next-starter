57932177b28207a9555168875eedb241514de2b7
Robert Szomolanyi <szomolanyi@gmail.com>
2019-05-28 19:13:42 +0200

    eslint

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 
-const {mergeSchemas, makeExecutableSchema} = require('graphql-tools')
+const { mergeSchemas, makeExecutableSchema } = require('graphql-tools');
 
-const CommentsSchema = require('./comments/Comments')
-const CommentsResolvers = require('./comments/resolvers')
-const UsersSchema = require("./users/Users")
-const UserResolvers = require("./users/resolvers")
+const CommentsSchema = require('./comments/Comments');
+const CommentsResolvers = require('./comments/resolvers');
+const UsersSchema = require('./users/Users');
+const UserResolvers = require('./users/resolvers');
 
 module.exports = mergeSchemas({
   schemas: [
-    makeExecutableSchema({typeDefs: CommentsSchema}),
-    makeExecutableSchema({typeDefs: UsersSchema}),
+    makeExecutableSchema({ typeDefs: CommentsSchema }),
+    makeExecutableSchema({ typeDefs: UsersSchema }),
   ],
-  resolvers: [CommentsResolvers, UserResolvers]
-})
+  resolvers: [CommentsResolvers, UserResolvers],
+});
