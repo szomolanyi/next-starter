7f7726fa62becabc64032ba6b8af8030538760a5
Robert Szomolanyi <szomolanyi@gmail.com>
2019-05-28 19:12:59 +0200

    cleaning

--- api/index.js
+++ api/index.js
@@ -1,18 +1,15 @@
 
 const {mergeSchemas, makeExecutableSchema} = require('graphql-tools')
 
-const TodosSchema = require('./todos/Todos')
 const CommentsSchema = require('./comments/Comments')
-const TodosResolvers = require('./todos/resolvers')
 const CommentsResolvers = require('./comments/resolvers')
 const UsersSchema = require("./users/Users")
 const UserResolvers = require("./users/resolvers")
 
 module.exports = mergeSchemas({
   schemas: [
-    makeExecutableSchema({typeDefs: TodosSchema}), 
     makeExecutableSchema({typeDefs: CommentsSchema}),
     makeExecutableSchema({typeDefs: UsersSchema}),
   ],
-  resolvers: [TodosResolvers, CommentsResolvers, UserResolvers]
+  resolvers: [CommentsResolvers, UserResolvers]
 })
