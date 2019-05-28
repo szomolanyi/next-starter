b498ed0afa6e249f51acd8f93b034495dc40010f
Robert Szomolanyi <szomolanyi@gmail.com>
2019-05-28 09:02:45 +0200

    eslint

--- next.config.js
+++ next.config.js
@@ -1,22 +1,21 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = (phase, { defaultConfig }) => {
+module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       /* development only config options here */
       target: 'serverless',
       env: {
-        GRAPHQL_URI: "http://localhost:3000/graphql"
-      }
-    }
+        GRAPHQL_URI: 'http://localhost:3000/graphql',
+      },
+    };
   }
-  else {
-    /* production */
-    return {
-      target: 'serverless',
-        env: {
-          GRAPHQL_URI: "https://next-starter.fomo1.now.sh/graphql"
-      }
-    }
-  }
-}
+
+  /* production */
+  return {
+    target: 'serverless',
+    env: {
+      GRAPHQL_URI: 'https://next-starter.fomo1.now.sh/graphql',
+    },
+  };
+};
