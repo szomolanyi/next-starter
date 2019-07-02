1a7d8a85f796b843d2d0c120a982b58639886ad7
Robert Szomolanyi <szomolanyi@gmail.com>
2019-07-02 09:53:29 +0200

    cleaning

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,9 +1,5 @@
 import Router from 'next/router';
-//import React, { useState } from 'react';
 import { graphql, compose } from 'react-apollo';
-import { errorHandler } from '../lib/hocs';
-
-//import { ManagedMutation } from '../lib/hocs';
 import { CREATE_USER } from '../lib/queries';
 import SignUpForm from './ui/SignUpForm';
 
@@ -17,29 +13,3 @@ export default compose(
     }),
   }),
 )(SignUpForm);
-
-/*
-const SignUp = ({ mutate }) => {
-  const [mutationError, setMutationError] = useState(null);
-  const enhancedMutate = data => mutate(data)
-    .then(() => {
-      Router.push('/');
-    })
-    .catch((error) => {
-      if (error.graphQLErrors) {
-        setMutationError(error.graphQLErrors[0].extensions.exception.message);
-      } else {
-        throw error;
-      }
-    });
-  return <SignUpForm signUp={enhancedMutate} signUpError={mutationError} />;
-};
-
-export default () => (
-  <ManagedMutation mutation={CREATE_USER}>
-    {
-      ({ mutate }) => <SignUp mutate={mutate} />
-    }
-  </ManagedMutation>
-);
-*/
