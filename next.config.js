4f26130b1afd92bb00f3a9a2958111cbad2a8ea8
Robert Szomolanyi <szomolanyi@gmail.com>
2019-12-04 16:44:00 +0100

    eslint

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const withSass = require('@zeit/next-sass');
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 module.exports = (phase) => {
   console.log(`phase: ${phase}`);
   console.log(`process.env.SERVER_URL: ${process.env.SERVER_URL}`);
-  console.log( `process.env.STANDALONE_GRAPHQL3: ${process.env.STANDALONE_GRAPHQL}`);
+  console.log(`process.env.STANDALONE_GRAPHQL3: ${process.env.STANDALONE_GRAPHQL}`);
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return withSass({
       env: {
