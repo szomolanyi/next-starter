18ad7db26344b28cbe56d75cf753472ebadec88b
Robert Szomolanyi <szomolanyi@gmail.com>
2019-12-06 08:43:29 +0100

    tested google auth with now dev

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 module.exports = (phase) => {
   console.log(`phase: ${phase}`);
   console.log(`process.env.SERVER_URL: ${process.env.SERVER_URL}`);
+  console.log(`process.env.SERVER_URL: ${process.env.APP_URL}`);
   console.log(`process.env.STANDALONE_GRAPHQL3: ${process.env.STANDALONE_GRAPHQL}`);
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return withSass({
