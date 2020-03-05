353bd59f3908abb5bd3a8c021f5c75cc44d586a6
Robert Szomolanyi <szomolanyi@gmail.com>
2020-03-05 14:45:06 +0100

    layout fix

--- src/components/tweets/Tweets.jsx
+++ src/components/tweets/Tweets.jsx
@@ -2,14 +2,14 @@ import TweetForm from './TweetForm';
 import TweetsFeed from './TweetsFeed';
 
 const Tweets = () => (
-  <div className="box">
+  <>
     <TweetForm
       initialValues={{
         text: '',
       }}
     />
     <TweetsFeed />
-  </div>
+  </>
 );
 
 export default Tweets;
