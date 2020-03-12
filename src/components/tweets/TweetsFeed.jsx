6bb47a7b330b4f26eed70d8bb3e06c95802d5db0
Robert Szomolanyi <szomolanyi@gmail.com>
2020-03-12 20:20:48 +0100

    tweets feed fetchMore

--- src/components/tweets/TweetsFeed.jsx
+++ src/components/tweets/TweetsFeed.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/react-hooks';
+import { useState, useEffect } from 'react';
 
-import Loading from '../ui/Loading';
+import LoadingSection from '../ui/LoadingSection';
 import AppError from '../ui/AppError';
 
 import { GET_TWEETS } from '../../queries';
@@ -12,21 +13,59 @@ const TweetsFeed = ({ filter }) => {
     loading,
     error,
     data,
-    fetchMore, //TODO add fetchMore
+    fetchMore,
   } = useQuery(GET_TWEETS, {
     variables: {
       limit: 10,
       filter,
     },
   });
-  if (loading) return <Loading />;
+  const [doRefetch, setRefetch] = useState(false);
+  useEffect(() => {
+    if (doRefetch === true) {
+      fetchMore({
+        query: GET_TWEETS,
+        variables: {
+          cursor: data.tweetsFeed.cursor,
+          limit: 10,
+          filter,
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          setRefetch(false);
+          return {
+            tweetsFeed: {
+              cursor: fetchMoreResult.tweetsFeed.cursor,
+              tweets: [
+                ...previousResult.tweetsFeed.tweets,
+                ...fetchMoreResult.tweetsFeed.tweets],
+              __typename: previousResult.tweetsFeed.__typename,
+            },
+          };
+        },
+      });
+    }
+  }, [doRefetch]);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      const tweetDiv = document.getElementById('tweet-feed-id');
+      if (window.scrollY + window.innerHeight >= tweetDiv.clientHeight + tweetDiv.offsetTop) {
+        setRefetch(true);
+      }
+    });
+  }, []);
+  if (loading) return <LoadingSection />;
   if (error) return <AppError error={error} />;
   return (
     <>
+      <div id="tweet-feed-id">
+        {
+          data.tweetsFeed.tweets.map((tweet) => (
+            <TweetDetail key={tweet._id} tweet={tweet} />
+          ))
+        }
+      </div>
       {
-        data.tweetsFeed.tweets.map((tweet) => (
-          <TweetDetail key={tweet._id} tweet={tweet} />
-        ))
+        doRefetch && <LoadingSection />
       }
     </>
   );
