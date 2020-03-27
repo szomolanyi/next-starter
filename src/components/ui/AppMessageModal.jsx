6757a8a8a7f675b256c98376d940b7bbf9597176
Robert Szomolanyi <szomolanyi@gmail.com>
2020-03-27 22:11:04 +0100

    fix no key

--- src/components/ui/AppMessageModal.jsx
+++ src/components/ui/AppMessageModal.jsx
@@ -17,7 +17,7 @@ const AppMessage = ({ appMessage, closeAppMessage }) => (
     {
       closeAppMessage && <button type="button" className="delete" aria-label="delete" onClick={() => closeAppMessage()} />
     }
-    {appMessage.messages.map((message) => <p>{message}</p>)}
+    {appMessage.messages.map((message, i) => <p key={i}>{message}</p>)}
   </div>
 );
 
