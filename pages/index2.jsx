065c36ccaa1a40aa1e362c4bbeda1bb253c2cc2f
Robert Szomolanyi <szomolanyi@gmail.com>
2020-10-09 21:47:22 +0200

    index2 hero

--- pages/index2.jsx
+++ pages/index2.jsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line
 import { withApollo } from '../client/apollo';
 import Layout from '../client/components/ui/layout';
+import { useUser } from '../client/hooks';
 
 const LoginPage = () => (
   <Layout>
-    <div className="section">
-      <div className="container has-text-centered">
-        <h1 className="title is-1">Welcome.</h1>
+    <div className="hero is-medium is-light bg-image">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title is-1">Welcome.</h1>
+        </div>
       </div>
     </div>
   </Layout>
