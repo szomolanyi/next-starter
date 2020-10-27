dcad80db14db1adf379cc403ccf605d25bcc22b5
Robert Szomolanyi <szomolanyi@gmail.com>
2020-10-27 21:25:19 +0100

    next v.10

--- next.config.js
+++ next.config.js
@@ -3,5 +3,7 @@ const withTM = require('next-transpile-modules')(['lodash-es', 'use-lodash-debou
 module.exports = () => {
   console.log(`process.env.NEXT_PUBLIC_SERVER_URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`);
   console.log(`process.env.APP_URL: ${process.env.APP_URL}`);
-  return withTM({});
+  return withTM({
+    analyticsId: 'xTCJiv9XFRCkUcLNeczVVWK9wn2',
+  });
 };
