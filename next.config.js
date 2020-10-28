aef6015aa2d0dc75c0414ba4e19d717494712d95
Robert Szomolanyi <szomolanyi@gmail.com>
2020-10-28 16:09:14 +0100

    cleaning

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withTM = require('next-transpile-modules')(['lodash-es', 'use-lodash-debounce-throttle']);
-
-module.exports = () => {
-  console.log(`process.env.NEXT_PUBLIC_SERVER_URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`);
-  console.log(`process.env.APP_URL: ${process.env.APP_URL}`);
-  return withTM({
-    analyticsId: 'xTCJiv9XFRCkUcLNeczVVWK9wn2',
-  });
-};
