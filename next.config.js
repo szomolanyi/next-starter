3c72e0c35e2394eb8f9a9b7140bafe6f5e08dccb
Robert Szomolanyi <szomolanyi@gmail.com>
2020-10-30 09:02:48 +0100

    handle missing Google auth params delete analytics Id

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = () => {
   console.log(`process.env.NEXT_PUBLIC_SERVER_URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`);
   console.log(`process.env.APP_URL: ${process.env.APP_URL}`);
   return withTM({
-    analyticsId: 'xTCJiv9XFRCkUcLNeczVVWK9wn2',
+    // analyticsId: 'xTCJiv9XFRCkUcLNeczVVWK9wn2',
   });
 };
--- server/passport.js
+++ server/passport.js
@@ -8,41 +8,46 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_APP_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_APP_SECRET,
-  callbackURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/google/callback`,
-  passReqToCallback: true,
-},
-async (request, accessToken, refreshToken, profile, done) => {
-  if (!profile.verified) {
-    return done(new Error('Google account is not verified'), false);
-  }
-  try {
-    const user = await User.findOne({ email: profile.email });
-    if (user && !user.isVerified) {
-      return done(new Error('Local email found, but it is not verified'), false);
+if (process.env.GOOGLE_APP_CLIENT_ID
+    && process.env.GOOGLE_APP_SECRET) {
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_APP_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_APP_SECRET,
+    callbackURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/google/callback`,
+    passReqToCallback: true,
+  },
+  async (request, accessToken, refreshToken, profile, done) => {
+    if (!profile.verified) {
+      return done(new Error('Google account is not verified'), false);
     }
-    if (!user) {
-      const newUser = new User({
-        firstName: profile.given_name,
-        lastName: profile.family_name,
-        google: profile,
-        email: profile.email,
-        isVerified: true,
-        hasLocalPassword: false,
-      });
-      await newUser.save();
-      return done(null, newUser);
+    try {
+      const user = await User.findOne({ email: profile.email });
+      if (user && !user.isVerified) {
+        return done(new Error('Local email found, but it is not verified'), false);
+      }
+      if (!user) {
+        const newUser = new User({
+          firstName: profile.given_name,
+          lastName: profile.family_name,
+          google: profile,
+          email: profile.email,
+          isVerified: true,
+          hasLocalPassword: false,
+        });
+        await newUser.save();
+        return done(null, newUser);
+      }
+      user.firstName = profile.given_name;
+      user.lastName = profile.family_name;
+      user.google = profile;
+      await user.save();
+      return done(null, user);
+    } catch (error) {
+      return done(error, false);
     }
-    user.firstName = profile.given_name;
-    user.lastName = profile.family_name;
-    user.google = profile;
-    await user.save();
-    return done(null, user);
-  } catch (error) {
-    return done(error, false);
-  }
-}));
+  }));
+} else {
+  console.warn('GOOGLE_APP_CLIENT_ID or GOOGLE_APP_SECRET environment variables are not defined ... google sosial auth will not work.');
+}
 
 export default passport;
